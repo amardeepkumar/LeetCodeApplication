3f09c9b00b7b5cad68ef431fd10d24c536155b5d
Amardeep Kumar <amardeepsit@gmail.com>
2025-10-31 10:29:44 +0530

    Solved Product of an array except self Solved Valid Plaindrome Co-authored-by: Vijayalakshmi I N  <vijimscse@gmail.com>

---
+++ app/src/main/java/com/amardeep/leetcodepractice/twopointer/TwoPointerSolution.kt
@@ -0,0 +1,59 @@
+package com.amardeep.leetcodepractice.twopointer
+
+class TwoPointerSolution {
+    //https://leetcode.com/problems/valid-palindrome/description/
+    fun isPalindrome(s: String): Boolean {
+
+        if (s.isEmpty()) return true
+        var i = 0
+        var j = s.length - 1
+        var str = s.lowercase()
+
+        while (i < j) {
+            when (str[i].toInt()) {
+                in 97..122 -> {}
+                in 48..57 -> {}
+                else -> {
+                    i++
+                    continue
+                }
+
+            }
+
+            when (str[j].toInt()) {
+                in 97..122 -> {}
+                in 48..57 -> {}
+                else -> {
+                    j--
+                    continue
+                }
+
+            }
+
+            if (str[i] != str[j]) {
+                return false
+            }
+            i++
+            j--
+        }
+        return true
+    }
+
+    //https://leetcode.com/problems/product-of-array-except-self/description/
+    fun productExceptSelf(nums: IntArray): IntArray {
+        var multiplier = 1
+        val result = IntArray(nums.size) {1}
+
+        nums.forEachIndexed() { index, element ->
+            result[index] = multiplier
+            multiplier = multiplier * element
+        }
+        multiplier = 1
+        for (i in nums.lastIndex downTo 0) {
+            result[i] = result[i] * multiplier
+            multiplier = multiplier * nums[i]
+        }
+
+        return result
+    }
+}
